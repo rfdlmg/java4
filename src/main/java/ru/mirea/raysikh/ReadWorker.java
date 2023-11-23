e036664a4d98291813f83cbb1213ee94557070a6
rfdlmg <rfdlmg@gmail.com>
2023-11-23 15:52:15 +0300

    wow it working

---
+++ src/main/java/ru/mirea/raysikh/ReadWorker.java
@@ -0,0 +1,24 @@
+package ru.mirea.raysikh;
+import java.io.BufferedReader;
+import java.io.IOException;
+public class ReadWorker implements Runnable{
+    private final BufferedReader reader;
+
+    public ReadWorker(BufferedReader reader) {
+        this.reader = reader;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
